13c8de1ec33f89b2d47d9969c151d15ef814b941
Dmytro Samodurov <dimasamodurov@gmail.com>
2017-09-04 14:05:55 +0300

    Add calendar styles.

--- app/javascript/packs/calendar.js
+++ app/javascript/packs/calendar.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import 'react-big-calendar/lib/css/react-big-calendar.css'
+
 import Calendar from 'components/calendar'
 
 document.addEventListener("DOMContentLoaded", e => {
